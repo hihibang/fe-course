c3cbc16f9a2d6870686ff5040a4eea8bc98afb2b
kim <0309098er@gmail.com>
2026-04-06 16:35:39 +0900

    260406

--- cgv-demo/js/cgv_support.js
+++ cgv-demo/js/cgv_support.js
@@ -1,58 +1,104 @@
+/**
+ * DOM 객체 생성 후 객체 호출
+ */
 window.addEventListener('DOMContentLoaded', () => {
-    let search_content = document.querySelector('#search_content')
-    let btnSearch = document.querySelector('#btnSearch')
+    /** 검색하기 버튼 이벤트 */
+    let search_content = document.querySelector('#search_content');
+    let btnSearch = document.querySelector('#btnSearch');
 
     btnSearch.addEventListener('click', () => {
         if(search_content.value.trim() === '') {
-            alert('내용 입력')
-            search_content.focus()
+            alert('검색할 내용을 입력해주세요');
+            search_content.focus();
+        } else {
+            //검색 진행
         }
-    })
-    
-    show()
-})
+    });
 
-//getJson 함수 생성;
-const getJson = async () => {
-    let response = await fetch('../data/support.json')
-    return response.json()
+    filterMenu('all');  //맨 처음 호출되는 테이블
+
+    let menuList = document.querySelectorAll('.filter-menu li a');
+    menuList.forEach(menu => {
+        menuList.forEach(menu => menu.id === 'all'? 
+            menuSelectColor(menu) : menuDefaultColor(menu));
+            
+        menu.addEventListener('click', () => {
+            menuList.forEach(menu => menuDefaultColor(menu));
+            menuSelectColor(menu);
+            filterMenu(menu.id);
+        });
+    });
+    
+}); //window event
+
+function menuDefaultColor(menu) {
+    menu.style.background = "rgb(137,137,135)";
+    menu.style.borderLeft = `1px solid var(rgb(137,137,135))`;
+    menu.style.borderBottom = `2px solid var(rgb(137,137,135))`;
 }
 
-//show 함수 생성
-const show = async () => {
-    let jsonData = await getJson() //panding: 나는 프로미스 타입이고 지금 Web API에서 너를 기다리고 있는 중이야
-    console.log(jsonData);
+function menuSelectColor(menu) {
+    menu.style.background = "rgb(251, 67, 87)";
+    menu.style.borderLeft = `1px solid var(rgb(251, 67, 87))`;
+    menu.style.borderBottom = `2px solid var(rgb(251, 67, 87))`;
+}
 
+//filterMenu 함수 생성
+async function filterMenu(type) {
+    let filterList = null;   
+
+    if(type === 'all') {
+        filterList = await getJson();
+    } else {
+        filterList = await filterData(type);
+    }    
+    createTable(filterList);
+}
+
+//filterData 함수 생성
+async function filterData(type) {
+    let data = await getJson();
+    return data.filter(item => item.type === type);
+}
+
+//support.json JSON 데이터 가져오기
+async function getJson() {
+    let response = await fetch("../data/support.json");
+    return response.json();
+}
+
+async function createTable(list) {
     let output = `
-        <table border=1>
-        <thead>
-            <tr>
-                <th>번호</th>
-                <th>구분</th>
-                <th>타이틀</th>
-                <th>생성일</th>
-                <th>조회수</th>
-            </tr>
-        </thead>
-        <tbody>
+        <table id='stable'>
+            <thead>
+                <tr>
+                    <th>번호</th>
+                    <th>구분</th>
+                    <th>제목</th>
+                    <th>등록일</th>
+                    <th>조회수</th>
+                </tr>
+            </thead>
+            <tbody>
             ${
-                jsonData.map((support, index) =>
-                    `<tr>
-                        <td>${index +1}</td>
-                        <td>${support.type}</td>
-                        <td>${support.title}</td>
-                        <td>${support.rdate}</td>
-                        <td>${support.hits}</td>
-                    </tr>`
-                ).join("") //문자열로 만들기
+                list.map((item, idx) => `
+                    <tr>
+                        <td>${idx+1}</td>
+                        <td>${item.type}</td>
+                        <td>${item.title}</td>
+                        <td>${item.rdate}</td>
+                        <td>${item.hits}</td>
+                    </tr>
+                `).join("")
             }
-        </tbody>
+            </tbody>
             <tfoot>
                 <tr>
-                    <td colspan="12344">1 2 3 4 5</td>
+                    <td colspan="5">1 2 3 4 5  >></td>
                 </tr>
-            </tfoot> 
+            </tfoot>
         </table>
-    `
-    document.querySelector('#before-table').insertAdjacentHTML('afterEnd', output)
+    `;
+    document.querySelector('#stable')?.remove();
+    document.querySelector('#before-table').insertAdjacentHTML('afterEnd', output);
 }
